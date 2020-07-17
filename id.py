0e0d72877991e20787e59aae5df9ed8dd34f49d7
mohankumar1u <45136005+mohankumar1u@users.noreply.github.com>
2020-07-17 18:07:05 +0530

    Update id.py

--- id.py
+++ id.py
@@ -1,14 +1,41 @@
 import paho.mqtt.client as mqtt #import the client1
-from flask import Flask
+from flask import Flask,request
+from flask_ngrok import run_with_ngrok
+import time
 app = Flask(__name__)
+#run_with_ngrok(app)
+broker_url = "127.0.0.1"
+broker_port = 1883
 
-@app.route('/')
+def on_connect(client, userdata, flags, rc):
+   print("Connected With Result Code ")
+
+
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.connect(broker_url, broker_port)
+
+
+
+@app.route('/',methods=["POST"])
 def hello_world():
-	broker_address="192.168.1.184" 
+	print("working")
+	request_data= request.json
+	print(request_data["topic"])
+	def on_message(client, userdata, message):
+   		print("Message Recieved: "+message.payload.decode())
+	   	return "sucesss"
+	client.on_message = on_message
+	client.subscribe(request_data["topic"], qos=1)
+	#broker_address="192.168.1.184" 
 	#broker_address="iot.eclipse.org" #use external broker
-	client = mqtt.Client("P1") #create new instance
-	client.connect("mqtt.eclipse.org", 1883, 60)
-	client.publish("house/main-light","OFF")#publish
-	return 'Success'
+	client.loop_start()
+	client.publish(topic=request_data["topic"], payload=request_data["paylaod"], qos=1, retain=False)
+	#time.sleep(4)
+	#client.loop_forever()
+	return "post" 
+
+
 if __name__ == '__main__':
-    app.run(host= '192.168.55.106', port=9000, debug=False)
+    app.run()
